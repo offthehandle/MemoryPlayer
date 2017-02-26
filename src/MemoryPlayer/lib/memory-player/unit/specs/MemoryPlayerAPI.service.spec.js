46a518c923d578efae10ff1c75741c31cb8b35b3
offthehandle <offthehandle@users.noreply.github.com>
2017-02-25 22:27:39 -0500

    Added MemoryPlayerAPI unit tests

---
+++ src/MemoryPlayer/lib/memory-player/unit/specs/MemoryPlayerAPI.service.spec.js
@@ -0,0 +1,38 @@
+﻿/**
+ * Max Volume method is not tested because it relies on internal jQuery event
+ */
+describe('MemoryPlayerAPI unit tests', function () {
+
+    beforeEach(module('MemoryPlayer'));
+
+    var $httpBackend;
+    var MemoryPlayerAPI;
+
+    beforeEach(inject(function (_$httpBackend_, _MemoryPlayerAPI_) {
+        $httpBackend = _$httpBackend_;
+
+        MemoryPlayerAPI = _MemoryPlayerAPI_;
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    describe('MemoryPlayerAPI unit test', function () {
+        it('should fetch the playlists json data', function () {
+
+            $httpBackend.expectGET('/lib/memory-player/dist/json/playlists.json').respond(200, playlists);
+
+            var playlistsResponse;
+
+            MemoryPlayerAPI.getPlaylists().then(function (response) {
+                playlistsResponse = response;
+            });
+
+            $httpBackend.flush();
+
+            expect(playlistsResponse).toEqual(playlists);
+        });
+    });
+});
